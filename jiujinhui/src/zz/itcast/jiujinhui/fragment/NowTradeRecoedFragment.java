bb8b6d76b259b491041f265a7a446e7b7162f820
chengongshun0809 <1574315939@qq.com>
2016-11-16 15:06:59 +0800

    交易曲线

--- jiujinhui/src/zz/itcast/jiujinhui/fragment/NowTradeRecoedFragment.java
+++ jiujinhui/src/zz/itcast/jiujinhui/fragment/NowTradeRecoedFragment.java
@@ -12,6 +12,7 @@ import javax.net.ssl.HttpsURLConnection;
 
 import lecho.lib.hellocharts.gesture.ContainerScrollType;
 import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.listener.LineChartOnValueSelectListener;
 import lecho.lib.hellocharts.model.Axis;
 import lecho.lib.hellocharts.model.AxisValue;
 import lecho.lib.hellocharts.model.Line;
@@ -33,6 +34,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
@@ -70,32 +72,46 @@ public class NowTradeRecoedFragment extends BaseFragment {
 		sp = getActivity().getSharedPreferences("user", 0);
 
 		ViewUtils.inject(this, view);
+		initLineChart();
+		lineChart.setOnValueTouchListener(new LineChartOnValueSelectListener() {
 
-		initLineChart();// 初始化
+			@Override
+			public void onValueDeselected() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onValueSelected(int arg0, int arg1, PointValue arg2) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getActivity(), arg1+"", 0).show();  
+				//line.setHasLabelsOnlyForSelected(true);
+				Log.e("点击节点", arg2 + "");
+			}
+		});
+
 		
 	}
 
 	private double nowprice;
 	private String nowcreatetime;
-	private LineChartView lineChartView;
+	// private LineChartView lineChartView;
 	private List<PointValue> mPointValues = new ArrayList<PointValue>();
 	private List<AxisValue> axisValuesX = new ArrayList<AxisValue>();
+	private List<AxisValue> axisValuesY = new ArrayList<AxisValue>();
 
 	// 画图表
 	protected void UpdateUI() {
 		// TODO Auto-generated method stub
 
 		getAxisLables();// 获取x轴的标注 // getAyisLables();// 获取y轴的刻度;
-		getAxisPoints();//
+
 		initLineChart();
-	
+		
 	}
 
-	
-
 	private void initLineChart() {
-		// TODO Auto-generated method stub
-		Line line = new Line(mPointValues);
+		line = new Line(mPointValues);
 
 		List<Line> lines = new ArrayList<Line>();
 		line.setShape(ValueShape.CIRCLE);// 折线图上每个数据点的形状 这里是圆形 （有三种
@@ -104,71 +120,71 @@ public class NowTradeRecoedFragment extends BaseFragment {
 											// ValueShape.DIAMOND）
 		line.setCubic(false);// 曲线是否平滑，即是曲线还是折线
 		line.setFilled(false);// 是否填充曲线的面积
-		line.setHasLabels(true);// 曲线的数据坐标是否加上备注
-		line.setHasLabelsOnlyForSelected(false);//
-		//点击数据坐标提示数据（设置了这个line.setHasLabels(true);就无效）
+		//line.setHasLabels(true);// 曲线的数据坐标是否加上备注
+		//
+		line.setHasPoints(true);
+
+		line.setPointRadius(3);
+		line.setHasLabels(false);
+		line.setHasLabelsOnlyForSelected(true);
+		// 点击数据坐标提示数据（设置了这个line.setHasLabels(true);就无效）
 		line.setHasLines(true);// 是否用线显示。如果为false 则没有曲线只有点显示
-		line.setHasPoints(false);// 是否显示圆点 如果为false 则没有原点只有点显示（每个数据点都是个大的圆点）
+		//line.setHasPoints(false);// 是否显示圆点 如果为false 则没有原点只有点显示（每个数据点都是个大的圆点）
 		line.setColor(Color.RED);
 		// 圆点半径
-		line.setPointRadius(2);
+		//line.setPointRadius(2);
 		line.setStrokeWidth(1);// 设置折线宽度
+		
 		lines.add(line);
 		LineChartData data = new LineChartData();
 		data.setLines(lines);
-		data.setValueLabelBackgroundEnabled(true);
-		data.setValueLabelsTextColor(Color.RED);
-		
+		//data.setValueLabelBackgroundEnabled(true);
+		data.setValueLabelsTextColor(Color.BLACK);
+          // data.setValueLabelTypeface(typeface);
 		// 坐标轴
 		Axis axisX = new Axis(); // X轴
 		axisX.setHasTiltedLabels(true); // X坐标轴字体是斜的显示还是直的，true是斜的显示
 		axisX.setTextColor(Color.GRAY); // 设置字体颜色
 		axisX.setName("实时详情数据"); // 表格名称
 		axisX.setTextSize(10);// 设置字体大小
-		axisX.setMaxLabelChars(6); // 最多几个X轴坐标，意思就是你的缩放让X轴上数据的个数7<=x<=mAxisXValues.length
+		axisX.setMaxLabelChars(10); // 最多几个X轴坐标，意思就是你的缩放让X轴上数据的个数7<=x<=mAxisXValues.length
 		axisX.setValues(axisValuesX); // 填充X轴的坐标值
 		data.setAxisXBottom(axisX); // x 轴在底部
 		// data.setAxisXTop(axisX); //x 轴在顶部
 		axisX.setHasLines(false); // x 轴分割
-		
+
 		// Y轴是根据数据的大小自动设置Y轴上限(在下面我会给出固定Y轴数据个数的解决方案)
-		Axis axisY = new Axis(); // Y轴
+		Axis axisY = new Axis().setHasLines(true); // Y轴
 		axisY.setName("");// y轴标注
 		axisY.setTextSize(10);// 设置字体大小
 		data.setAxisYLeft(axisY); // Y轴设置在左边
 		// data.setAxisYRight(axisY); //y轴设置在右边
-		
 
 		// 设置行为属性，支持缩放、滑动以及平移
 		lineChart.setInteractive(false);
-		//lineChart.setZoomType(ZoomType.HORIZONTAL_AND_VERTICAL);
+		// lineChart.setZoomType(ZoomType.HORIZONTAL_AND_VERTICAL);
 		// lineChart.setMaxZoom((float) 2);//最大方法比例
-		lineChart.setScrollEnabled(false);
+	
 		lineChart.setLineChartData(data);
-		lineChart.setValueTouchEnabled(true);
-		lineChart.setFocusableInTouchMode(true);
-		lineChart.setVisibility(View.VISIBLE);
-		//lineChart.startDataAnimation();
 		
-	}
+		lineChart.setValueSelectionEnabled(true);
+		lineChart.setVisibility(View.VISIBLE);
+		// lineChart.startDataAnimation();
+		/*
+		 * Viewport v = new Viewport(lineChart.getMaximumViewport()); v.left =
+		 * 0; v.right= 5; lineChart.setCurrentViewport(v);
+		 */
 
-	// 图中每个点的显示
-	private void getAxisPoints() {
-
-		for (int i = 0; i < priceList.size(); i++) { //
-			mPointValues.add(new PointValue(i, priceList.get(i)));
-			// Log.e("eeeeeeeeeeeeee", "ssss");
-
-		}
 	}
 
 	// X轴刻度的显示
 	private void getAxisLables() { // TODO Auto- generated method
-		for (int i = 0; i < timeList.size(); i++) {
+		for (int i = 0; i < priceList.size(); i++) {
+			mPointValues.add(new PointValue(i, firstpriceList.get(i)));
+			axisValuesX.add(new AxisValue(i).setValue(i).setLabel(
+					firsttimeList.get(i)));
+			axisValuesY.add(new AxisValue(i).setValue(firstpriceList.get(i)));// 添加Y轴显示的刻度值
 
-			axisValuesX.add(new AxisValue(i).setLabel(timeList.get(i)));
-
-			// Log.e("eeeeeeeeeeeeee", "sssfsdfsas");
 		}
 
 	}
@@ -176,8 +192,12 @@ public class NowTradeRecoedFragment extends BaseFragment {
 	public static List<String> timeList;
 	public static List<Float> priceList;
 
+	public static List<String> firsttimeList;
+	public static List<Float> firstpriceList;
+
 	// String[] times = { "09:00", "11:30", "15:00", "22:00" };
 	private String stateString;
+	private Line line;
 
 	protected void refreshdata() {
 		// TODO Auto-generated method stub
@@ -222,27 +242,33 @@ public class NowTradeRecoedFragment extends BaseFragment {
 			jsonArraylist = jsonObject.getJSONArray("todaydeal");
 			timeList = new ArrayList<String>();
 			priceList = new ArrayList<Float>();
-			for (int i = 0; i < jsonArraylist.length(); i++) {
+			firsttimeList = new ArrayList<String>();
+			firstpriceList = new ArrayList<Float>();
+			JSONObject object2 = (JSONObject) jsonArraylist.get(0);
+			// 初始指导价的值时间
+			double firstprice = object2.getDouble("price");
+			String firsttime = object2.getString("createtime");
+			firstpriceList.add((float) (firstprice / 100));
+			firsttimeList.add(firsttime.substring(10, 16));
+			for (int i = 1; i < jsonArraylist.length(); i++) {
 				JSONObject object = (JSONObject) jsonArraylist.get(i);
-				nowprice = object.getDouble("price");
+
 				stateString = object.getString("state");
 				// timeList.add(nowcreatetime.substring(10,16));
+
 				if ("0".equals(stateString)) {
 
-					priceList.add((float) (nowprice / 100));
-
+					nowprice = object.getDouble("price");
 					nowcreatetime = object.getString("createtime");
 					timeList.add(nowcreatetime.substring(10, 16));
-					Log.e("wewweew", priceList + "");
-
+					priceList.add((float) (nowprice / 100));
 				}
 
-				DecimalFormat df = new DecimalFormat("#0.00");
-
-				// createtime不能转换为double
-
 			}
-
+			firstpriceList.addAll(priceList);
+			firsttimeList.addAll(timeList);
+			Log.e("wewweew", firstpriceList + "");
+			Log.e("eeeeeeeeeeeeee", firsttimeList + "");
 			Message message = new Message();
 			message.what = 1;
 			handler.sendMessage(message);
